dbaea8c7cec3e317698ed48bdfc54fed17c3c3ff
Paul Gerritsen <paul188297@gmail.com>
2024-05-14 14:23:39 -0600

    styled login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
+import { Form, Button, Row, Col } from 'react-bootstrap'
 
 const Login = () => {
   return (
-    <form>
-        <h1>Login</h1>
-        <label htmlFor="username">Username: </label>
-        <input name='username'></input>
-        <br/>
-        <label htmlFor="password">Password: </label>
-        <input name='password' type='password'></input>
-        <br/>
-        <button>Submit</button>
-    </form>
+    <Form>
+        <h1 className='mb-4'>Login</h1>
+        <Form.Group as={Row} className='mb-5'>
+            <Form.Label column xs={{offset: 2, span: 2}} className='fs-4'>Email address</Form.Label>
+            <Col xs={6}>
+                <Form.Control type='email' placeholder='Email' size='lg'/>
+            </Col>
+        </Form.Group>
+        <Form.Group as={Row} className='mb-5'>
+            <Form.Label column xs={{offset: 2, span: 2}} className='fs-4'>Password</Form.Label>
+            <Col xs={6}>
+                <Form.Control type='password' placeholder='Password' size='lg'/>
+            </Col>
+        </Form.Group>
+        <Row>
+            <Button variant='secondary' size='lg' as={Col}xs={{offset: 2, span: 8}}>Sign In</Button>
+        </Row>
+    </Form>
   )
 }
 
